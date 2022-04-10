88c60cd8a9d4732496d2fe5c9f4822cf3893ab14
saiprasad <rsaiprasad4@gmail.com>
2022-04-10 15:31:18 +0530

    done

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,14 +3,12 @@ const logger = require('./logger')
 
 const router = express.Router();
 
+//1.Create an API for GET /movies that returns a list of movies. Define an array of movies in your code and return the value in response.
 
 router.get('/movies', function (req, res) {
     let movies = ['rang de basnasti', 'the shining', 'lord of the rings', 'bartman begins'];
     res.send(movies);
 });
-
-
-
 
 
 //2.Create an API GET /movies/:indexNumber (For example GET /movies/1 is a valid request and it should return the movie in your array at index 1). You can define an array of movies again in your api
@@ -31,11 +29,21 @@ router.get("/movies/:abcd", function (req, res) {
   if (x > movies.length || x < 0) {
     return res.status(404).send("!!!Invalid Index. Please Enter the Correct Index");
   } else {
-    for (i = 0; i < movies.length; i++) {
-      if (i == x) {
-        return res.send(movies[i]);
-      }
-    }
+      ///Using Map Function
+
+    movies.map(function(e,i,array){
+        if( i == x ){
+            return res.send(e);
+        }
+    })
+
+    // Using for loop
+    
+    // for (i = 0; i < movies.length; i++) {
+    //   if (i == x) {
+    //     return res.send(movies[i]);
+    //   }
+    // }
   }
 });
 
@@ -43,8 +51,7 @@ router.get("/movies/:abcd", function (req, res) {
 
 router.get("/movies1",function(req,res){
  res.status(200).send(x);
-   
-})
+   })
 
 
 /////4.Write api GET /films/:filmId where filmId is the value received in request path params. Use this value to return a movie object with this id. In case there is no such movie present in the array, return a suitable message in the response body. Example for a request GET /films/3 should return the movie object 
@@ -72,22 +79,22 @@ router.get("/movies2/:movieindex",function(req,res){
       
  let x = req.params.movieindex;
  // from map function(4.1)
- movies.map(function(obj){
-      if ( obj.id == x ){
-           return res.send(obj);
-      }
-     })
+ movies.map(function (y) {
+   if (y.id == x) {
+     return res.send(y);
+   }
+ });
 
 
 
-     //from for loop(4.2)
-    //  for(i=0;i<movies.length;i++){
-    //      if( x > movies.length ) {
-    //          return res.send("No Such Movie Exists on this Index");
-    //      }else if( x == i ){
-    //          return res.send(movies[i]);
-    //      }
-    //  }
+//from for loop(4.2)
+//  for(i=0;i<movies.length;i++){
+//      if( x > movies.length ) {
+//          return res.send("No Such Movie Exists on this Index");
+//      }else if( x == i ){
+//          return res.send(movies[i]);
+//      }
+//  }
 })
 
 

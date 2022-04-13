bdd4358578d1353442db5216d99c3899cc09f052
saiprasad <rsaiprasad4@gmail.com>
2022-04-13 08:59:25 +0530

    done

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,13 +1,53 @@
 const { count } = require("console")
-const BookModel= require("../models/bookModel")
+const BookModel1= require("../models/bookModel1.js")
 
 const createBook= async function (req, res) {
-    let data= req.body
-    let savedData= await BookModel.create(data)
-    res.send({msg: savedData})
+    let data = req.body;
+    let savedBookData = await BookModel1.create(data);
+    res.send({ message: savedBookData });
+  }
+
+const getBookData= async function (req, res) {
+    let allBookmodels = await BookModel1.find();                   //fine
+    res.send({ message: allBookmodels });
+  }
+
+  const getBookDataOnlyAuthorAndName = async function (req, res) {
+    let allBookmodels = await BookModel1.find().select({ bookName: 1, authorName: 1, _id: 0 });         ///fine
+    res.send({ message: allBookmodels });
+  }
+
+  const getBooksInYear =async function (req, res) {
+    let inYear= req.body.year                                                                             //fine
+      let allBookmodels= await BookModel1.find({year: inYear})
+      res.send({msg: allBookmodels})
+  }
+
+
+ const getParticularBooks = async function (req, res) {
+    let key= req.query
+    let ParticularBooks= await BookModel1.find(key).select({_id:0, _v: 0,createdAt:0, updatedAt:0})    // fine
+    res.send({msg: ParticularBooks})
+    
 }
 
-const getBooksData= async function (req, res) {
-}
+
+const getXINRBooks= async function (req, res) {
+    // let INRBooks= await bookModels.find({prices: {indianPrice: {$in : [100, 200, 500] } }})
+    let INRBooks= await BookModel1.find({"prices.indianPrice" : {$in:["100INR", "200INR","500INR"]}} )     ///fine
+      res.send({ message: INRBooks });                                                                     
+  }
+
+
+
+  const getRandomBooks =async function (req, res) {
+    let allBookmodels = await BookModel1.find({ $or: [{ totalpages: { $gt: 500 } }, { stockAvailable: true }] });
+     res.send({ message: allBookmodels });
+   }
+module.exports.getBookData= getBookData
 module.exports.createBook= createBook
-module.exports.getBooksData= getBooksData
+module.exports.getBookDataOnlyAuthorAndName= getBookDataOnlyAuthorAndName
+module.exports.getBooksInYear= getBooksInYear
+module.exports.getParticularBooks= getParticularBooks
+module.exports.getXINRBooks= getXINRBooks
+module.exports.getRandomBooks= getRandomBooks
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,66 +1,29 @@
 const express = require('express');
 const router = express.Router();
 const bookModels = require("../models/bookModel1.js");
+const bookcontroller = require ("../controllers/bookController.js")
 
 
 /// 1. Get All Book Data From User.
-router.post("/createBookData", async function (req, res) {
-  let data = req.body;
-  let savedBookData = await bookModels.create(data);
-  res.send({ message: savedBookData });
-})
-
-
+router.post("/createBookData", bookcontroller.createBook)
 
 /// 2. Fetch All Data That Has Been Entered By User.
-router.get("/getBookData", async function (req, res) {
-  let allBookmodels = await bookModels.find();                   //fine
-  res.send({ message: allBookmodels });
-})
-
-
+router.get("/getBookData", bookcontroller.getBookData)
 
 /// 3. Get filtered Book Data Only by Author And Book Name. 
-router.get("/getBookDataOnlyAuthorAndName", async function (req, res) {
-  let allBookmodels = await bookModels.find().select({ bookName: 1, authorName: 1, _id: 0 });         ///fine
-  res.send({ message: allBookmodels });
-})
-
-
-
+router.get("/getBookDataOnlyAuthorAndName",bookcontroller.getBookDataOnlyAuthorAndName )
 
 /// 4.takes year as input in post request and gives list of all books published that year
-router.get("/getBooksInYear", async function (req, res) {
-  let inYear= req.query.year                                                                             //fine
-    let allBookmodels= await bookModels.find({year: inYear}).select({_id:0, _v: 0,createdAt:0, updatedAt:0})
-    res.send({msg: allBookmodels})
-})
-
-
+router.post("/getBooksInYear",bookcontroller.getBooksInYear )
 
 /// 5.1  e.g if body had { name: “hi”} then you would fetch the books with this name
 /// 5.2  if body had { year: 2020} then you would fetch the books in this year
-router.get("/getParticularBooks", async function (req, res) {
-    let key= req.query
-    let ParticularBooks= await bookModels.find(key).select({_id:0, _v: 0,createdAt:0, updatedAt:0})   // not sure
-    res.send({msg: ParticularBooks})
-    
-})
-
-
+router.get("/getParticularBooks", bookcontroller.getParticularBooks)
 
 /// 6. request to return all books who have an Indian price tag of “100INR” or “200INR” or “500INR” 
-router.get("/getXINRBooks", async function (req, res) {
-  let INRBooks= await bookModels.find({prices: {indianPrice: {$in : [100, 200, 500] } }})
-    res.send({ message: INRBooks });                                                                    //not sure
-})
-
-
+router.get("/getXINRBooks",bookcontroller.getXINRBooks )
 
 // 7 . returns books that are available in stock or have more than 500 pages 
-router.get("/getRandomBooks", async function (req, res) {
- let allBookmodels = await bookModels.find({ $or: [{ totalpages: { $gt: 500 } }, { stockAvailable: true }] });
-  res.send({ message: allBookmodels });
-})                                                           //fine
+router.get("/getRandomBooks",bookcontroller.getRandomBooks )                                                           //fine
 
 module.exports = router;

b48e9d4f778d97d282493936d312b1262b8847bf
saiprasad <rsaiprasad4@gmail.com>
2022-04-14 09:29:29 +0530

    done completly

---
+++ src/models/book_model.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose")
+
+const bookschema = new mongoose.Schema({
+    name : String,
+    author_id : 
+    {
+        type : Number,
+        required : true
+        },
+        price : Number,
+        ratings : Number
+    },
+    {timestamps : true })
+
+
+module.exports = mongoose.model('book1',bookschema)
+
+
+
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,41 +1,17 @@
 const express = require('express');
 const router = express.Router();
-// const UserModel= require("../models/userModel.js")
-const UserController= require("../controllers/userController")
-const BookController= require("../controllers/bookController")
+const controller = require("../controllers/book_author_controllers.js")
 
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
+router.post("/createUser", controller.createNewAuthors  )
 
-router.post("/createUser", UserController.createUser  )
+router.post("/createBook", controller.createNewBook  )
 
-router.get("/getUsersData", UserController.getUsersData)
+router.get("/allBooks", controller.allBooks)
 
-router.post("/createBook", BookController.createBook  )
+router.get("/updateBookPrice", controller.updateBookPrice)
 
-router.get("/getBooksData", BookController.getBooksData)
+router.get("/authorNames", controller.authorNames)
 
-router.post("/updateBooks", BookController.updateBooks)
-router.post("/deleteBooks", BookController.deleteBooks)
 
-//MOMENT JS
-const moment = require('moment');
-router.get("/dateManipulations", function (req, res) {
-    
-    // const today = moment();
-    // let x= today.add(10, "days")
-
-    // let validOrNot= moment("29-02-1991", "DD-MM-YYYY").isValid()
-    // console.log(validOrNot)
-    
-    const dateA = moment('01-01-1900', 'DD-MM-YYYY');
-    const dateB = moment('01-01-2000', 'DD-MM-YYYY');
-
-    let x= dateB.diff(dateA, "days")
-    console.log(x)
-
-    res.send({ msg: "all good"})
-})
 
 module.exports = router;

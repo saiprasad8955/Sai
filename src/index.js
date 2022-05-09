13fe4c65c688543d10eca7b841bd32020f0326b4
saiprasad <rsaiprasad4@gmail.com>
2022-05-09 16:29:26 +0530

    done

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const route = require("./route/route");
+const bodyParser = require("body-parser")
+const app = express();
+const chalk = require("chalk");
+const mongoose = require("mongoose")
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+mongoose.connect(
+    "mongodb+srv://Sai0047:rXxgqYKPqwnhcXX7@cluster0.qptsw.mongodb.net/group23DatabaseBook"
+,{useNewUrlParser:true})
+.then(()=>{console.log(chalk.bgCyan("MongoDB is Successfully Connected"))})
+.catch((err)=> {console.log(err.message)});
+
+
+app.use('/',route);
+
+app.listen( 3000, ()=>{console.log(chalk.bgMagenta("Express App is running on Port 3000"))})
+
+
---
+++ src/models/userModel.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+title:{
+    type:String,
+    required:true,
+    enum:["Mr","Mrs","Miss"],
+    trim:true
+},
+
+name:{
+    type:String,
+    required:true,
+    trim:true,
+},
+
+phone:{
+    type:String,
+    required:true,
+    unique:true,
+    trim:true
+},
+
+email:{
+    type:String,
+    required:true,
+    unique:true,
+    trim:true
+},
+
+
+},{timestamps:true});
+
+module.exports =mongoose.model("User",userSchema)

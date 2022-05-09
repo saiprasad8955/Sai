5fd9d451f6513108be85058402aa1c4dd5f3523e
vishalpkale <102423910+vishalpkale@users.noreply.github.com>
2022-05-09 17:09:07 +0530

    "user login"

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,3 +1,39 @@
 
 const userModel = require("../models/userModel")
+const jwt=require
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const userLogin=async function(req,res){
+    const{email,password}=req.body;
+    const chkUser=await userModel.findOne({email:email,password:password})
+    if(!chkUser){
+        return res.status(404).send({status: false, message: "Email or Password doesn't match"})
+    }
+    const token= 
+}
